98ac5ef205dc3defe2c2f832f35ce9043aee790f
Munna-MR7 <munna.cse6.bu@gmail.com>
2025-10-23 02:06:17 +0600

    Day1-ClassObjectArrayList

--- src/main/java/day1ClassObject/Main.java
+++ src/main/java/day1ClassObject/Main.java
@@ -1,2 +1,41 @@
-package day1ClassObject;public class Main {
+package day1ClassObject;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Person> person = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 1; i <= n; i++) {
+            Person p = new Person();
+            //Scanner scc = new Scanner(System.in);
+
+            System.out.println("Enter name: ");
+            p.name = sc.nextLine();
+
+            System.out.println("Enter age: ");
+            p.age = sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("Enter address: ");
+            p.address = sc.nextLine();
+
+            person.add(p);
+
+            //System.out.println(p);
+//            p.introduce();
+
+        }
+        for(Person p : person){
+            System.out.println(p);
+            p.introduce();
+        }
+    }
 }
--- src/main/java/day1ClassObject/Person.java
+++ src/main/java/day1ClassObject/Person.java
@@ -1,2 +1,21 @@
-package day1ClassObject;public class Person {
+package day1ClassObject;
+public class Person {
+    String name;
+    int age;
+    String address;
+    void introduce(){//String name, String address
+        System.out.println();
+
+        System.out.println("I am " + name + ", I Live in " + address);
+
+    }
+    @Override
+    public String toString(){
+        return "Person{" +
+                "name = " +name + "\n"+
+                ", age = " + age + "\n"+
+                ", address = " + address +
+                "}";
+    }
 }
+
